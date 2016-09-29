9a514ec723ac92ec2b259b4a44e5e2d357a889cd
Akshat <akshat@mit.edu>
2016-09-28 23:33:37 -0400

    plotting stuff

--- HW1/hw1.py
+++ HW1/hw1.py
@@ -8,148 +8,176 @@ import grad_desc as gd
 
 plt.style.use('fivethirtyeight')
 plt.rcParams['axes.facecolor'] = 'white'
+plt.rcParams['axes.edgecolor'] = 'black'
+plt.rcParams['figure.autolayout'] = 'true'
 
 def getWML(phis, X, t):
-	M,N = len(phis), len(X)
-	Phi = np.asmatrix([[phis[j](X[i]) for j in range(M)] for i in range(N)])
-	PSI = ((Phi.T*Phi).I)*(Phi.T)
-	t = vmat(t)
-	return PSI*t
+    M,N = len(phis), len(X)
+    Phi = np.asmatrix([[phis[j](X[i]) for j in range(M)] for i in range(N)])
+#   print("DETERMINTANT OF MATRIX:", np.linalg.det(Phi.T*Phi))
+    PSI = ((Phi.T*Phi).I)*(Phi.T)
+    t = vmat(t)
+    return PSI*t
 
 def getWRidge(phis, X, t, lbd):
-	M,N = len(phis), len(X)
-	Phi = np.asmatrix([[phis[j](X[i]) for j in range(M)] for i in range(N)])
-	PSI = ((lbd*np.identity(M)+Phi.T*Phi).I)*(Phi.T)
-	t = np.asmatrix(t).T
-	return PSI*t
+    M,N = len(phis), len(X)
+    Phi = np.asmatrix([[phis[j](X[i]) for j in range(M)] for i in range(N)])
+    PSI = ((lbd*np.identity(M)+Phi.T*Phi).I)*(Phi.T)
+    t = np.asmatrix(t).T
+    return PSI*t
 
 def apply(f, n):
-	return lambda x: f(x,n)
+    return lambda x: f(x,n)
 
 def linearSum(w, phis):
-	return (lambda x: sum([float(w[i])*phis[i](x) for i in range(len(w))]))
+    return (lambda x: sum([float(w[i])*phis[i](x) for i in range(len(w))]))
 
 def getBasis(M, f):
-	return [apply(f,n) for n in range(M)]
+        if f == cosn:
+            return [apply(f,n) for n in range(1,M+1)]
+        return [apply(f,n) for n in range(M)]
 
 def poly(x,n):
-	return x**n
+    return x**n
 
 def cosn(x,n):
-	return np.cos(n*math.pi*x)
+    return np.cos(n*math.pi*x)
 
 def vmat(v):
-	return np.asmatrix(v).T
+    return np.asmatrix(v).T
 
 def SSE(f, X, Y):
-	ret = 0 
-	for (x,y) in zip(X,Y):
-		ret += (f(x) - y)**2
-	return ret
+    ret = 0 
+    for (x,y) in zip(X,Y):
+        ret += (f(x) - y)**2
+    return ret
 
 def SSE_vec_fn(phi, X, Y):
-	def sse(w):
-		n,m = len(X), len(phi)
-		dif = [(sum([w[i]*phi[i](X[j]) for i in range(m)]) - Y[j]) for j in range(n)]
-		return np.sum(np.square(dif))
-	return sse
+    def sse(w):
+        n,m = len(X), len(phi)
+        dif = [(sum([w[i]*phi[i](X[j]) for i in range(m)]) - Y[j]) for j in range(n)]
+        return np.sum(np.square(dif))
+    return sse
 
 def SSE_grad_fn(phi, X, Y):
-	def grad(w):
-		n,m = len(X), len(phi)
-		dif = [sum([2*(sum([w[i]*phi[i](X[j]) for i in range(m)]) - Y[j])*phi[k](X[j]) for j in range(n)]) for k in range(m)]
-		return dif
-	return grad
+    def grad(w):
+        n,m = len(X), len(phi)
+        dif = [sum([2*(sum([w[i]*phi[i](X[j]) for i in range(m)]) - Y[j])*phi[k](X[j]) for j in range(n)]) for k in range(m)]
+        return dif
+    return grad
 
-def P2(M, F):
-	X,Y = load2.getData(False)
-	wml = getWML(getBasis(M,F), X, Y)
-	est = linearSum(wml,getBasis(M,F))
-	xml   = np.arange(0,1,0.01)
-	yml   = np.array(est(xml))
-	yac   = np.cos(math.pi*xml) + np.cos(2*math.pi*xml)
+def P2(M, F, F2 = None):
+    X,Y = load2.getData(False)
+    wml = getWML(getBasis(M,F), X, Y)
+    print(wml)
+    est = linearSum(wml,getBasis(M,F))
+    xml   = np.arange(0,1,0.01)
+    yml   = np.array(est(xml))
+    yac   = np.cos(math.pi*xml) + np.cos(2*math.pi*xml)
 
-	plt.plot(X,Y,'o', markersize = 12, markeredgewidth=2 ,markeredgecolor= 'b', markerfacecolor = 'none')
-	plt.plot(xml,yml)
-	plt.plot(xml,yac)
-	plt.xlabel('x')
-	plt.ylabel('y')
-	axes = plt.gca()
-	axes.set_ylim([-2,3])
-	plt.savefig('P2_'+str(M)+'_'+str(F.__name__)+'.png')
-	plt.close()
+    plt.plot(xml,yml, label = 'Polynomial basis regression')
+    plt.plot(xml,yac, label = 'Actual function')
+    if F2:
+        wml2 = getWML(getBasis(M,F2), X, Y)
+        est = linearSum(wml2,getBasis(M,F2))
+        yml   = np.array(est(xml))
+        plt.plot(xml,yml, label = 'Cosine basis regression')
+    plt.plot(X,Y,'o', markersize = 12, markeredgewidth=2 ,markeredgecolor= '#1e8c19', markerfacecolor = 'none', label = 'Training data')
+    plt.xlabel('x')
+    plt.ylabel('y')
+    #plt.title('Linear Regression (M = '+str(M-1)+')')
+    plt.title('Polynomial and Cosine basis functions (M=8)')
+    axes = plt.gca()
+    axes.set_ylim([-2,3])
+    axes.grid(False)
+    plt.legend()
+    plt.savefig('P2_'+str(M)+'_'+str(F.__name__)+'.png')
+    plt.close()
 
-	return wml
+    return wml
 
 def P3(M, lbd, dataFunc, plot = True, part1 = False):
-	F = poly
-	X,Y = dataFunc()
-	wml = getWRidge(getBasis(M,F), X,Y, lbd)
-	est = linearSum(wml,getBasis(M,F))
-	xml   = np.arange(min(X),max(X),0.01)
-	yml   = np.array(est(xml))
+    F = poly
+    X,Y = dataFunc()
+    wml = getWRidge(getBasis(M,F), X,Y, lbd)
+    est = linearSum(wml,getBasis(M,F))
+    xml   = np.arange(min(X),max(X),0.01)
+    yml   = np.array(est(xml))
 
-	fname = 'P3_'+str(M)+'_'+str(lbd)+'_'+str(dataFunc.__name__)+'.png'
+    fname = 'P3_'+str(M)+'_'+str(lbd)+'_'+str(dataFunc.__name__)+'.png'
 
-	print(fname, SSE(est,X,Y))
+    print(fname, SSE(est,X,Y))
 
-	if plot:
-		plt.plot(X,Y,'o')
-		plt.plot(xml,yml)
-		if part1:
-			yac   = np.cos(math.pi*xml) + np.cos(2*math.pi*xml)
-			plt.plot(xml,yac)
-		plt.xlabel('x')
-		plt.ylabel('y')
+    if plot:
+        plt.plot(X,Y,'o')
+        plt.plot(xml,yml)
+        if part1:
+            yac   = np.cos(math.pi*xml) + np.cos(2*math.pi*xml)
+            plt.plot(xml,yac)
+        plt.xlabel('x')
+        plt.ylabel('y')
 
-		plt.savefig(fname)
-		plt.close()
+        plt.savefig(fname)
+        plt.close()
 
-	return est
+    return est
 
 def validate(f, dataFunc, plot = True):
-	X,Y = dataFunc()
-	xml   = np.arange(min(X),max(X),0.01)
-	yml   = np.array(f(xml)).reshape(-1)
+    X,Y = dataFunc()
+    xml   = np.arange(min(X),max(X),0.01)
+    yml   = np.array(f(xml)).reshape(-1)
 
-	fname = 'Testing_'+str(dataFunc.__name__)+'.png'
-	err = SSE(f,X,Y)
-	print(fname, err)
+    fname = 'Testing_'+str(dataFunc.__name__)+'.png'
+    err = SSE(f,X,Y)
+    print(fname, err)
 
-	if plot:
-		plt.plot(X,Y,'o')
-		plt.plot(xml,yml)
-		plt.xlabel('x')
-		plt.ylabel('y')
+    if plot:
+        plt.plot(X,Y,'o')
+        plt.plot(xml,yml)
+        plt.xlabel('x')
+        plt.ylabel('y')
 
-		plt.savefig(fname)
-		plt.close()
+        plt.savefig(fname)
+        plt.close()
 
-	return err
+    return err
 
 def P2_2(M, F, point = None):
-	X,Y = load2.getData(False)
+    X,Y = load2.getData(False)
 
-	phis = getBasis(M,poly)
-	wml = getWML(phis, X, Y)
-	sse = SSE_vec_fn(phis,X,Y)
-	grad = SSE_grad_fn(phis,X,Y)
+    phis = getBasis(M,poly)
+    wml = getWML(phis, X, Y)
+    sse = SSE_vec_fn(phis,X,Y)
+    grad = SSE_grad_fn(phis,X,Y)
 
-	wml = np.asarray(wml.T)[0]
-	if point == None:
-		point = wml
-	print("wml: ",wml)
-	print("SSE: ",sse(point))
-	print("Analytic Grad: ",grad(point))
-	ng = gd.GD([0]*M, sse)
-	print("Numeric Grad:",ng.compute_gradient(point, eps =1e-8))
+    wml = np.asarray(wml.T)[0]
+    if point == None:
+        point = wml
+    print("***************************************")
+    print(M)
+    print("wml: ",wml)
+    #print("SSE: ",sse(point))
+    #print("Analytic Grad: ",grad(point))
+    #print("Analytic Grad Norm: ",np.linalg.norm(grad(point)))
+    ng = gd.GD([0]*M, sse)
+    #print("Numeric Grad:",ng.compute_gradient(point, eps =1e-8))
+    #print("Numeric Grad Norm:",np.linalg.norm(ng.compute_gradient(point, eps =1e-8)))
 
-	phiX = np.asarray([[phis[i](x) for i in range(M)] for x in X])
-	sgd = gd.SGD(phiX, Y)
-	#sgd_log = sgd.step(np.ones(M), stochastic = True, minibatch_size = 1, ftol = 1e-9, gtol = 1e-5, stepSize = 0.1)
-	gd_log = sgd.step(np.zeros(M), stochastic = False, ftol = 1e-9, gtol = 1e-5, stepSize = 0.01)[-1]
-	#print("Stochastic: ", sgd_log[-1])
-	print("Batch GD: ", gd_log)
+    phiX = np.asarray([[phis[i](x) for i in range(M)] for x in X])
+    sgd = gd.SGD(phiX, Y)
+    sgd_log = sgd.step(np.random.rand(M), stochastic = True, minibatch_size = 1, ftol = 1e-9, gtol = 1e-3, stepSize = 0.1)
+    gd_log = sgd.step(np.random.rand(M), stochastic = False, ftol = 1e-9, gtol = 1e-5, stepSize = 0.02)
+    print("Stochastic: ", sgd_log[-1])
+    #print("Stochastic second last: ", sgd_log[-2])
+    print("Stochastic len: ", len(sgd_log))
+    #print("Closed form grad at Stoch: ", grad(sgd_log[-1][0]))
+
+    #print("Numeric grad at Stoch: ", ng.compute_gradient(sgd_log[-1][0], eps =1e-8))
+    #print("In code grad at Stoch: ", sgd.compute_gradient(sgd_log[-1][0]))
+    #print("In code numeric grad at Stoch: ", sgd.compute_numerical_gradient(sgd_log[-1][0]))
+
+    print("Batch GD: ", gd_log[-1])
+    print("Batch GD len: ", len(gd_log))
 
 #stuff for problem 3
 #f = P3(10, 0.5, load3.regressAData)
@@ -158,7 +186,43 @@ def P2_2(M, F, point = None):
 #Problem 2.2
 #P2_2(5,poly)
 
-P2(1,poly)
-P2(2,poly)
-P2(4,poly)
-P2(11,poly)
+def gen_p2():
+    P2(1,poly)
+    P2(2,poly)
+    P2(4,poly)
+    P2(6,poly)
+    P2(8,poly)
+    P2(11,poly)
+
+def gen_p22():
+    P2_2(5,poly)
+
+def gen_p24():
+    wml = P2(8,cosn, poly)
+    lefts = range(1,9)
+
+#    plt.xlabel('i')
+    #plt.ylabel('$w_i$')
+    plt.title('Most likely weights for M=8')
+    plt.bar(lefts, wml, align = 'center', tick_label = lefts)
+    axes = plt.gca()
+    axes.set_ylim([0,1])
+    axes.grid(False)
+    plt.savefig('bar1.png')
+    plt.close()
+
+    true = np.zeros(8)
+    true[0] = 1
+    true[1] = 1
+#    plt.xlabel('i')
+#    plt.ylabel('$w_i$')
+    plt.title('Actual weights')
+    plt.bar(lefts, true, align = 'center', tick_label = lefts)
+    axes = plt.gca()
+    axes.grid(False)
+    axes.set_ylim([0,1])
+    plt.savefig('bar2.png')
+    plt.close()
+
+
+gen_p24()
